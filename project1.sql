614576cfd0ae49478761b7b4db7473a66f1c5bde
doradu8030 <30680014+doradu8030@users.noreply.github.com>
2017-09-23 21:37:54 -0400

    Add files via upload

---
+++ project1.sql
@@ -0,0 +1,31 @@
+﻿DROP TABLE IF EXISTS tournamentfinal;
+
+CREATE TABLE tournamentfinal
+(
+  id character(10),	
+  playername character(30) NOT NULL,
+  payerstate character(30),
+  totnumpoints character(10),
+  round1 character(10),
+  round2 character(10),
+  round3 character(10),
+  round4 character(10),
+  round5 character(10),
+  round6 character(10),
+  round7 character(10),
+  prerating integer,
+  avgprerateoppon integer,
+  CONSTRAINT pk_id PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+COPY tournamentfinal(id,playername,payerstate,totnumpoints,round1,round2,round3,round4,round5,
+  round6,round7,prerating,avgprerateoppon)
+FROM 'C:\Users\Dora\Downloads\Assignment2\tournamentfinal.csv' CSV DELIMITER ',' HEADER;
+
+
+
+SELECT * FROM tournamentfinal;
